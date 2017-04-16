c470ad889098c6e6c02a47a518283d67966a26f2
lenmorld <lenmorld@gmail.com>
2017-04-16 09:13:41 -0400

    ch6_jwt1_users

--- jwt_sample/server.js
+++ jwt_sample/server.js
@@ -1,17 +1,30 @@
 var express = require('express');
 var jwt = require('jwt-simple');
+var _ = require('lodash');      // better replacement to Underscore
 var app = express();
-
 app.use(require('body-parser').json());
 
+var users = [{username: 'lenny', password: 'pass'}];
 var secretKey = 'supersecretkey';
 
+function findByUsername(username) {
+    return _.find(users, {username: username});
+}
+
+function validateUser(user, password) {
+    return user.password === password;
+}
+
 app.post('/session', function (req, res) {
-    var username = req.body.username;
+    // var username = req.body.username;
     // TODO: validate password
+    var user = findByUsername(req.body.username);
+    if (!validateUser(user, req.body.password)) {
+        return res.send(401);           // unauthorized
+    }
+
     var token = jwt.encode(
-        {username: username},
-        secretKey
+        {username: user.username}, secretKey
     );
     res.json(token);
 });
